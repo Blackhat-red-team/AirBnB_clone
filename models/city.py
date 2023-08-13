b375d1f97f5db21a56878a84e8a9c63d3f54e6ee
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-08-13 21:25:51 +0300

    d

--- models/city.py
+++ models/city.py
@@ -8,6 +8,3 @@ class City(BaseModel):
     name = ""
 
 
-    def __init__(self, *args, **kwargs):
-       
-        super().__init__(*args, **kwargs)
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,64 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+# test cases for base class
+import unittest
+from models.base_model import BaseModel
 from models.city import City
+import models.base_model
+import models.city
+import inspect
+import datetime
+from time import sleep
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(unittest.TestCase):
+    
+    def setUp(self):
+       
+        self.c = City()
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+    def test_doc_city(self):
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertIsNotNone(models.city.__doc__, 'no docs for module')
+        for name, method in inspect.getmembers(City, inspect.isfunction):
+         self.assertIsNotNone(method.__doc__, f"{name} has no docs")
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+
+
+
+
+    def test_init_city(self):
+        
+        self.assertEqual(type(self.c.id), str)
+        self.assertEqual(type(self.c.updated_at), datetime.datetime)
+        self.assertEqual(type(self.c.created_at), datetime.datetime)
+
+    def test_save_city(self):
+        
+        current_updatedAt = self.c.updated_at
+        self.c.save()
+        self.assertNotEqual(current_updatedAt, self.c.updated_at)
+
+        
+        with self.assertRaises(TypeError):
+            self.c.save(13)
+
+    def test_to_dict_city(self):
+       
+        self.c.name = "NYC"
+        dict1 = self.c.to_dict()
+
+       
+        self.assertEqual(type(dict1['name']), str)
+        self.assertEqual(type(dict1['__class__']), str)
+        self.assertEqual(dict1['__class__'], "City")
+        self.assertEqual(type(dict1['updated_at']), str)
+        self.assertEqual(type(dict1['id']), str)
+        self.assertEqual(type(dict1['created_at']), str)
+
+       
+        with self.assertRaises(TypeError):
+            self.c.to_dict('str')
+
+
+if __name__ == '__main__':
+    unittest.main()
